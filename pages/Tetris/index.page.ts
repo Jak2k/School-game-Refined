9cb0c110d99c65f3232eed892b4b3e6100b75c01
lachface <69272214+lachface@users.noreply.github.com>
2023-09-26 08:34:09 +0000

    WIP

--- pages/Tetris/index.page.ts
+++ pages/Tetris/index.page.ts
@@ -4,7 +4,6 @@ import { type ServerHTML, ServerMeta, Init } from "../../renderer/types";
 
 //define classList in the server site to avoid error
 
-
 if (typeof document === "undefined") {
   // @ts-ignore
   global.document = {
@@ -14,20 +13,47 @@ if (typeof document === "undefined") {
   };
 }
 
-const squares = Array.from(document.querySelectorAll(".grid div"));
+let squares: HTMLDivElement[] = [];
 const startBtn = document.querySelector("#start-button")!;
-const width = 1;
 let nextRandom = 0;
+let currentPosition = 0;
+let currentTetrominoe = 0;
+let currentColor = 0;
+let filledsquares = [];
 const theTetrominoes = [
-  [1, width + 1, width * 2 + 1, 2], // lTetromino
-  [0, width, width + 1, width * 2 + 1], // zTetromino
-  [1, width, width + 1, width + 2], // tTetromino
-  [0, 1, width, width + 1], // oTetromino
-  [1, width + 1, width * 2 + 1, width * 3 + 1], // iTetromino
+  [
+    currentPosition + 0,
+    currentPosition + 10,
+    currentPosition + 20,
+    currentPosition + 21,
+  ], // lTetromino
+  [
+    currentPosition + 0,
+    currentPosition + 1,
+    currentPosition + 11,
+    currentPosition + 12,
+  ], // zTetromino
+  [
+    currentPosition + 0,
+    currentPosition + 1,
+    currentPosition + 2,
+    currentPosition + 11,
+  ], // tTetromino
+  [
+    currentPosition + 0,
+    currentPosition + 1,
+    currentPosition + 10,
+    currentPosition + 11,
+  ], // oTetromino
+  [
+    currentPosition + 0,
+    currentPosition + 10,
+    currentPosition + 20,
+    currentPosition + 30,
+  ], // iTetromino
 ];
-let currentPosition = 4;
-let random = Math.floor(Math.random() * theTetrominoes.length);
-let current = theTetrominoes[random];
+
+const colors = ["orange", "red", "purple", "green", "blue", "yellow", "cyan"];
 
 function createGrid() {
   const grid = document.querySelector(".grid")!;
@@ -36,6 +62,9 @@ function createGrid() {
     div.innerHTML = i.toString();
     div.classList.add("grid-item");
     div.id = i.toString();
+    div.style.backgroundColor = "black";
+    div.style.borderBlockColor = "white";
+    squares.push(div);
     grid.appendChild(div);
   }
 }
@@ -54,70 +83,74 @@ export const serverMeta: ServerMeta = () => {
   };
 };
 
-function displayShape() {
-  const displaySquares = document.querySelectorAll(".mini-grid div");
-  displaySquares.forEach((square) => {
-    square.classList.remove("tetromino");
-  });
-  theTetrominoes[nextRandom].forEach((index) => {
-    displaySquares[0 + index].classList.add("tetromino");
-  });
-}
-
-function draw() {
-  current.forEach((index) => {
-    squares[currentPosition + index].classList.add("tetromino");
-  });
-}
-
-function undraw() {
-  current.forEach((index) => {
-    squares[currentPosition + index].classList.remove("tetromino");
-  });
-}
-
-function freeze() {
-  if (
-    current.some((index) =>
-      squares[currentPosition + index + width].classList.contains("taken")
-    )
-  ) {
-    current.forEach((index) =>
-      squares[currentPosition + index].classList.add("taken")
-    );
-    random = nextRandom;
-    nextRandom = Math.floor(Math.random() * theTetrominoes.length);
-    current = theTetrominoes[random];
-    currentPosition = 4;
-    draw();
+export const init: Init = () => {
+  createGrid();
+  startBtn.addEventListener("click", () => {
+    newTetrominoe();
     displayShape();
+    let timerId = setInterval(moveDown, 1000);
+  });
+  document.addEventListener("keydown", (e) => {
+    switch (e.key) {
+      case "ArrowLeft":
+        moveLeft();
+        break;
+      case "ArrowRight":
+        moveRight();
+        break;
+      // case "ArrowDown":
+      //  moveDown();
+      //  break;
+      default:
+        break;
+    }
+  });
+};
+
+function newTetrominoe() {
+  currentTetrominoe = Math.floor(Math.random() * theTetrominoes.length);
+  currentColor = Math.floor(Math.random() * colors.length);
+}
+
+function moveLeft() {
+  if (currentPosition % 10 === 0) {
+    return;
   }
+  currentPosition--;
+  undraw();
+  displayShape();
+}
+
+function moveRight() {
+  if (currentPosition % 10 === 9) {
+    return;
+  }
+  currentPosition++;
+  undraw();
+  displayShape();
 }
 
 function moveDown() {
   undraw();
-  currentPosition += width;
-  draw();
-  freeze();
+  currentPosition += 10;
+  displayShape();
 }
-export const init: Init = () => {
-  createGrid();
-  for (let i = squares.length - 1; i >= 0; i--) {
-    console.log(i);
-  }
-  let timerId: number | null = null;
-  startBtn.addEventListener("click", () => {
-    if (timerId) {
-      clearInterval(timerId);
-      timerId = null;
-    } else {
-      draw();
-      timerId = setInterval(() => {
-        moveDown();
-        currentPosition += 1;
-      }, 1000);
-      nextRandom = Math.floor(Math.random() * theTetrominoes.length);
-      displayShape();
+
+function displayShape() {
+  theTetrominoes[currentTetrominoe].forEach((index: any) => {
+    index += currentPosition;
+    squares[index].style.backgroundColor = colors[currentColor];
+  });
+}
+
+function undraw() {
+  theTetrominoes[currentTetrominoe].forEach((index: any) => {
+    if (index >= 190) {
+      newTetrominoe();
+      return;
     }
   });
-};
+  squares.forEach((index: any) => {
+    squares[index.id].style.backgroundColor = "black";
+  });
+}
