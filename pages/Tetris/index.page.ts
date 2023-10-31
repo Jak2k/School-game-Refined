fb01a22954d7876a24506cfdf551fbb1462b8bca
lachface <69272214+lachface@users.noreply.github.com>
2023-10-31 09:36:14 +0000

    WIP

--- pages/Tetris/index.page.ts
+++ pages/Tetris/index.page.ts
@@ -10,7 +10,7 @@ if (typeof document === "undefined") {
     createElement: () => {},
   };
 }
-
+// filledsquares = [[123, color]]
 let squares: HTMLDivElement[] = [];
 const startBtn = document.querySelector("#start-button")!;
 let currentPosition = 0;
@@ -53,6 +53,29 @@ const theTetrominoes = [
   ], // iTetromino
 ];
 
+let Tetrominoelines = [
+  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
+  [20, 21, 22, 23, 24, 25, 26, 27, 28, 29],
+  [30, 31, 32, 33, 34, 35, 36, 37, 38, 39],
+  [40, 41, 42, 43, 44, 45, 46, 47, 48, 49],
+  [50, 51, 52, 53, 54, 55, 56, 57, 58, 59],
+  [60, 61, 62, 63, 64, 65, 66, 67, 68, 69],
+  [70, 71, 72, 73, 74, 75, 76, 77, 78, 79],
+  [80, 81, 82, 83, 84, 85, 86, 87, 88, 89],
+  [90, 91, 92, 93, 94, 95, 96, 97, 98, 99],
+  [100, 101, 102, 103, 104, 105, 106, 107, 108, 109],
+  [110, 111, 112, 113, 114, 115, 116, 117, 118, 119],
+  [120, 121, 122, 123, 124, 125, 126, 127, 128, 129],
+  [130, 131, 132, 133, 134, 135, 136, 137, 138, 139],
+  [140, 141, 142, 143, 144, 145, 146, 147, 148, 149],
+  [150, 151, 152, 153, 154, 155, 156, 157, 158, 159],
+  [160, 161, 162, 163, 164, 165, 166, 167, 168, 169],
+  [170, 171, 172, 173, 174, 175, 176, 177, 178, 179],
+  [180, 181, 182, 183, 184, 185, 186, 187, 188, 189],
+  [190, 191, 192, 193, 194, 195, 196, 197, 198, 199],
+];
+
 const colors = ["orange", "red", "purple", "green", "blue", "yellow", "cyan"];
 
 function createGrid() {
@@ -85,7 +108,11 @@ export const serverMeta: ServerMeta = () => {
 export const init: Init = () => {
   createGrid();
   startBtn.addEventListener("click", () => {
+    if (running === true) {
+      return;
+    }
     newTetrominoe();
+
     displayShape();
     let timerId = setInterval(moveDown, 1000);
     running = true;
@@ -114,6 +141,28 @@ export const init: Init = () => {
   });
 };
 
+function deleteLine() {
+
+}
+
+function checkLines() {
+  let tempfilledsquares: number[] = [];
+  filledsquares.forEach((forms) => {
+    forms[0].forEach((index: number) => {
+      tempfilledsquares.push(index);
+    });
+  });
+  tempfilledsquares.sort();
+  for (let i = Tetrominoelines.length - 1; i >= 0; i--) {
+    //hier letztes mal aufgehört
+    if (tempfilledsquares.includes(Tetrominoelines[i][0])) {
+      Tetrominoelines.splice(i, 1);
+    }
+  }
+}
+
+
+
 function newTetrominoe() {
   currentTetrominoe = Math.floor(Math.random() * theTetrominoes.length);
   currentColor = Math.floor(Math.random() * colors.length);
@@ -123,9 +172,11 @@ function saveTetrominoe() {
   theTetrominoes[currentTetrominoe][currentrotation].forEach((index: any) => {
     filledsquares.push([index + currentPosition, colors[currentColor]]);
   });
+  currentrotation = 0;
   currentPosition = 0;
   newTetrominoe();
   displayShape();
+  checkLines();
 }
 
 function moveLeft() {
@@ -133,10 +184,10 @@ function moveLeft() {
   for (let i = 0; i < theTetrominoes[currentTetrominoe].length; i++) {
     const index =
       currentPosition + theTetrominoes[currentTetrominoe][currentrotation][i];
-      
-      if (currentPosition % 10 === 0) {
-        return;
-      }
+
+    if (index % 10 === 0) {
+      return;
+    }
 
     filledsquares.forEach((filledsquare) => {
       if (filledsquare[0] === index - 1) {
@@ -209,21 +260,50 @@ function rotatetetrominoe() {
     temprotation++;
   }
   let end = false;
-  for(let i = 0; i < theTetrominoes[currentTetrominoe][temprotation].length; i++) {
-    if (theTetrominoes[currentTetrominoe][temprotation][i] + currentPosition >= 190) {
+  for (
+    let i = 0;
+    i < theTetrominoes[currentTetrominoe][temprotation].length;
+    i++
+  ) {
+    if (
+      theTetrominoes[currentTetrominoe][temprotation][i] + currentPosition >=
+      190
+    ) {
       end = true;
     }
     filledsquares.forEach((filledsquare) => {
-      if (filledsquare[0] === theTetrominoes[currentTetrominoe][temprotation][i] + currentPosition) {
+      if (
+        filledsquare[0] ===
+        theTetrominoes[currentTetrominoe][temprotation][i] + currentPosition
+      ) {
         end = true;
       }
-      if (theTetrominoes[currentTetrominoe][temprotation][i] + currentPosition < 0) {
+      if (
+        theTetrominoes[currentTetrominoe][temprotation][i] + currentPosition <
+        0
+      ) {
         end = true;
       }
-      if ((theTetrominoes[currentTetrominoe][currentrotation][i] + currentPosition) % 10 === 0 && (theTetrominoes[currentTetrominoe][temprotation][i] + currentPosition) % 10 === 9) {
+      if (
+        (theTetrominoes[currentTetrominoe][currentrotation][i] +
+          currentPosition) %
+          10 ===
+          0 &&
+        (theTetrominoes[currentTetrominoe][temprotation][i] + currentPosition) %
+          10 ===
+          9
+      ) {
         end = true;
       }
-      if ((theTetrominoes[currentTetrominoe][currentrotation][i] + currentPosition) % 10 === 9 && (theTetrominoes[currentTetrominoe][temprotation][i] + currentPosition) % 10 === 0) {
+      if (
+        (theTetrominoes[currentTetrominoe][currentrotation][i] +
+          currentPosition) %
+          10 <=
+          9 &&
+        (theTetrominoes[currentTetrominoe][temprotation][i] + currentPosition) %
+          10 ===
+          0
+      ) {
         end = true;
       }
     });
