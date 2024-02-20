b766dd71533d2264f9ad638c05b85da0c7a103e6
lachface <69272214+lachface@users.noreply.github.com>
2024-02-20 09:27:12 +0000

    fixed bugs and UI

--- pages/index.page.ts
+++ pages/index.page.ts
@@ -3,10 +3,12 @@ import { type ServerHTML, ServerMeta, Init } from "../renderer/types";
 
 export const serverHTML: ServerHTML = () => `
 <div id="games">
-  <button class="game" onclick="windows.location.href = '/2048'">2048</button>
-  <button class="game" onclick="window.location.href = '/Tetris'">Tetris</button>
+  <button class="game" onclick="window.location.href = '/2048'">2048</button>
+  <button class="game" onclick="window.location.href = '/tetris'">Tetris</button>
   <button class="game" onclick="window.location.href = '/calculator'">Rechner</button>
   <button class="game" onclick="window.location.href = '/snake'">Snake</button>
+  <button class="game" onclick="window.location.href = '/minesweeper'">Mine- sweeper</button>
+  <button class="game" onclick="window.location.href = '/gameoflife'">Game of Life</button>
 </div>
 <div id="debug"></div>
 `;
